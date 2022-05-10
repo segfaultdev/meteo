2a7ab7d9c2c4a4afab51823f6e9c1735b0b7405d
segfaultdev <72017506+segfaultdev@users.noreply.github.com>
2022-05-10 10:33:09 +0200

    Add files via upload

--- patata.ino
+++ patata.ino
@@ -1,5 +1,5 @@
 // ESP8266 -> pins 2(arduino's RX, esp's TX), 3(arduino's TX, esp's RX) and 4(CH_PD)
-// DHT11 -> pin 3
+// DHT11 -> pin 5
 // LCD1602 -> pins SDA and SCL
 // LDR(?) -> pin A0
 
@@ -15,15 +15,40 @@
 #define PIN_DHT     5
 #define PIN_LDR     A0
 
-#define SERVER_PORT "2121"
-#define SSID "YourSSID"
-#define PASSWORD "YourPassword"
+#define SERVER_PORT "8000"
+#define SSID "MURCIAEDUCA-DOC"
+#define PASSWORD "eeccmurcia2017"
+
+SoftwareSerial soft_serial(PIN_SOFT_RX, PIN_SOFT_TX);
+SerialESP8266wifi wifi(soft_serial, soft_serial, PIN_CH_PD, Serial);
+
+dht11 sensor;
 
 void setup() {
+  soft_serial.begin(9600);
+
+  wifi.setTransportToTCP();
+  wifi.endSendWithNewline(true);
   
+  if (wifi.begin()) {
+    wifi.connectToAP(SSID, PASSWORD);
+    wifi.startLocalServer(SERVER_PORT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  static WifiConnection *connections;
+  wifi.checkConnections(&connections);
+  
+  for (int i = 0; i < MAX_CONNECTIONS; i++) {
+    if (connections[i].connected) {
+      WifiMessage msg = wifi.getIncomingMessage();
+      sensor.read(PIN_DHT);
+      
+      char data[256];
+      sprintf(data, "%d %d %d", analogRead(A0), sensor.humidity, sensor.temperature);
+      
+      wifi.send(msg.channel, data);
+    }
+  }
 }
